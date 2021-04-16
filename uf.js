fb51d9714d81f032f23ca00e2a575f7e05ac1736
Francisco <victoriafrancis2009@gmail.com>
2021-04-16 17:58:04 -0400

    complete

--- uf.js
+++ uf.js
@@ -1,10 +1,34 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
+    let date = process.argv[2].split('/')
+    let year = date[2]
+    let month = parseInt(date[1])
+    let day = parseInt(date[0])
+    let url = "https://www.sii.cl/valores_y_fechas/uf/uf2021.htm";
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto("https://homer.sii.cl/");
-    await page.screenshot({path: 'sii.png'});
+    await page.goto(url);
 
+    await page.select('#sel_anyo', year)
+    try {
+        let ufs = await page.$eval('#mes_all > div > table', table =>{
+            return table.querySelector('tbody').textContent
+        })
+
+        let ufs_ = ufs.trim().split('\n')/*.filter(v => {
+            return v != ""
+        })*/
+        let UF = []
+        for (let index = 0; index < 31; index++) {
+            let salto = index*15
+            UF.push(ufs_.slice(salto,salto+15))
+        }
+        console.log(UF[day-1][month].trim())
+    
+        
+    } catch (error) {
+        console.error("Año no encontrado")
+    }
     await browser.close();
 })();
